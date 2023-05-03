86a6867b404775ca805184719d632e1bbb505080
AfiaAniebiet <aniebietafia87@gmail.com>
2023-05-03 17:24:08 +0000

    Fixed pycodestyle errors

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 """
-Create a measure_time function with integers n and max_delay as arguments that measures the
-total execution time for wait_n(n, max_delay), and returns total_time / n.
-Your function should return a float. Use the time module to measure an approximate elapsed time.
+Create a measure_time function with integers
+n and max_delay as arguments that measures the
+total execution time for wait_n(n, max_delay),
+and returns total_time / n.Your function should
+return a float. Use the time module to measure
+an approximate elapsed time.
 """
 
 from time import time
@@ -13,7 +16,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """ Return execution time for wait_n given `n` and `max_delay`. """
+    """ Return execution time for wait_n given 
+    `n` and `max_delay`. """
     time_0 = time()
     run(wait_n(n, max_delay))
     time_1 = time()
